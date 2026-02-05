9517d967e230e5f64c4d1fe476e5a9a67bc8ec2f
Subham Bose <132132082+Subham401@users.noreply.github.com>
2026-02-05 23:00:31 +0530

    git init

--- src/main/java/com/company/admindashboard/service/UserService.java
+++ src/main/java/com/company/admindashboard/service/UserService.java
@@ -4,15 +4,18 @@ import com.company.admindashboard.dto.UserRequest;
 import com.company.admindashboard.entity.UserEntity;
 import com.company.admindashboard.mapper.UserMapper;
 import com.company.admindashboard.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    private final PasswordEncoder passwordEncoder;
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void createUser(UserRequest request){
@@ -20,6 +23,8 @@ public class UserService {
             throw new IllegalArgumentException(" Username already present");
         }
         UserEntity entity = userMapper.toEntity(request);
+        String encoded  =passwordEncoder.encode((request.getPassword()));
+        entity.setPassword(encoded);
         userRepository.save(entity);
 
 
